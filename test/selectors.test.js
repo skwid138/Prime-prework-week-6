3a7548c44f160b94c41cdb42fccc72bb001c5236
Millie Walsh <millie@primeacademy.io>
2017-02-21 21:51:07 -0600

    more assignment clean up with tests

--- test/selectors.test.js
+++ test/selectors.test.js
@@ -1,6 +1,6 @@
-describe('application', function() {
+describe('basemode 1_selectors assignment', function() {
   beforeEach(function () {
-    fixture.setBase('5-1/src');
+    fixture.setBase('assignment/1_selectors');
     fixture.load('index.html');
   });
 
@@ -8,31 +8,23 @@ describe('application', function() {
     fixture.cleanup();
   });
 
-  // T1
-  it('should show header h1', function() {
+  // 1
+  it('should not show header h1', function() {
     // make sure DOM is loaded
     onReady();
-    assert.equal($('header h1').is(':visible'), true, 'header h1 element should be visible');
+    assert.equal($('header h1').is(':visible'), false, 'header h1 element should not be visible');
   });
 
-  // T1
-  it('should keep other h1s hidden', function() {
+  // 1
+  it('should keep other h1s visable', function() {
     onReady();
-    assert.equal($('main h1').is(':visible'), false, 'other headers should be hidden');
+    assert.equal($('main h1').is(':visible'), true, 'other headers should be visable');
   });
 
-  // T3
+  // 2
   it('should change background-color of span with id step-two to yellow', function() {
     onReady();
     var spanBg = $('#step-two').css('background-color') == 'rgb(255, 255, 0)';
     assert.equal(spanBg, true, 'background-color should be yellow');
   });
 });
-
-// -- Test util functions --
-// function that iterates over cardImg elements and validates they are displayed
-// function validateShown(){
-//   $('header h1').each(function(index, el) {
-//     console.log(el);
-//   });
-// }
