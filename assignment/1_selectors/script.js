96b7b5a367f0bdc43e06f1b58b5139014ae7fa10
Millie Walsh <millie@primeacademy.io>
2017-02-21 21:20:46 -0600

    clean up assignment comments and readme to correspond

--- assignment/1_selectors/script.js
+++ assignment/1_selectors/script.js
@@ -9,26 +9,24 @@ See the README.md file for instructions
 var onReady = function() {
   console.log('DOM ready');
 
-  // T1
-  // write your code for task 1 here.
+  // 1 - hide just the h1 inside header
+  // write your code for question 1 here.
 
-  // T2
-  // write your code for task 2 here.
+  // 2 - change the background color of 'step-two' to yellow
+  // write your code for question 2 here.
 
-  // T2 Bonus (optional)
-  // write your code for task 3 here.
+  // 3 - fadout example elements
+  // 3 bonus - wait one second and fade back in
+  // write your code for question 3 here.
 
-  // T3
-  // write your code for task 3 bonus here.
+  // 4 - select all odd list items and change to blue
+  // write your code for question 4 here.
 
-  // T4
-  // write your code for task 4 here.
+  // 5 - select the last list item with a pseudo selector
+  // write your code for question 5 here.
 
-  // T5
-  // write your code for task 5 here.
-
-  // T6
-  // write your code for task 6 here.
+  // 6 - select the last list item with traversal
+  // write your code for question 6 here.
 };
 
 // When the DOM is loaded, our code in the onReady function will run.
