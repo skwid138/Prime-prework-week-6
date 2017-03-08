8f9e576ee4c5b6f170b06e82a63de5109089fcff
Millie Walsh <millie@primeacademy.io>
2017-03-08 14:30:13 -0600

    quick fixes

--- assignment/3_memory-game/script.js
+++ assignment/3_memory-game/script.js
@@ -3,8 +3,8 @@ console.log('Test Sourced');
 var onReady2 = function() {
   console.log('memory game doc ready');
 
-  $('#revealHide').on('click', revealHide);
-  $('.cardDiv').click(singleClickFunc);
+  //TODO Add your code below to attach your event listeners to functions
+  
 };
 
 // on document ready run the onReady2 function
@@ -12,12 +12,14 @@ $(document).ready(onReady2);
 
 // revealHide function hides and shows all cards
 function revealHide() {
-  console.log('called revealHide');
-  $('.cardImg').toggle();
+
+  //TODO add your code here to get the desired functionality
+
 }
 
 // singleClickFunc function hides and shows an indivdual card
 function singleClickFunc() {
-  console.log('called singleClickFunc');
-  $(this).children().toggle();
+
+  //TODO add your code here to get the desired functionality
+
 }
