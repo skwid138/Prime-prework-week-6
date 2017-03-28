381202ed737849ffde62da9b7f10aa29b0f2f1d3
Huck Brock <huckbrock@gmail.com>
2017-03-28 14:12:32 -0500

    adds sinon with the karma-better-sinon-chai package

--- test/selectors.test.js
+++ test/selectors.test.js
@@ -30,4 +30,23 @@ describe('basemode 1_selectors assignment', function() {
     var spanBg = $('#step-two').css('background-color') == 'rgb(255, 255, 0)';
     assert.equal(spanBg, true, 'background-color should be yellow');
   });
+
+  // 3 - fadout example elements
+  // 3 bonus - wait one second and fade back in
+  // $('.example-list').fadeOut(1500).delay(1000).fadeIn(500);
+  // it('should fadeOut the example-list', function() {
+  //   // sinon.stub(jQuery.prototype, "fadeOut");
+  //   var spy = sinon.spy($.prototype, "fadeOut");
+  //
+  //
+  //
+  //   // onReady();
+  //   spy();
+  //   spy.should.have.been.called();
+  //   spy.reset();
+  //   // assert.equal($('.example-list').is(':visible'), false, 'not visible breifly').to;
+  // });
+
+
+
 });
