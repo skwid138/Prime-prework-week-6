005ea95876c8881fa857023497d6638c74ec8f12
skwid138 <skwid138@gmail.com>
2017-08-27 17:41:31 -0500

    fixxed show/hide bug

--- assignment/3_memory-game/script.js
+++ assignment/3_memory-game/script.js
@@ -6,7 +6,6 @@ var onReady2 = function() {
   //TODO Add your code below to attach your event listeners to functions
   $('.cardDiv').click(singleClickFunc);
   $('#revealHide').click(revealHide);
-  // $('.cardImg').css('visibility', 'hidden');
   // $('.cardImg').hide();
 };
 
@@ -18,10 +17,10 @@ function revealHide() {
 
   //TODO add your code here to get the desired functionality
   console.log('func: revealHide');
-  $('.cardImg').toggle();
-  // if( $('img.cardImg').children().is(":visible") ) {
-  //   $('img.cardImg').children().hide();
-  // } else ( $('.cardImg').toggle() );
+  // $('.cardImg').toggle();
+  if( $('.cardImg').is(":visible") ) {
+    $('.cardImg').hide();
+  } else ( $('.cardImg').toggle() );
 }
 
 // singleClickFunc function hides and shows an indivdual card
@@ -30,7 +29,4 @@ function singleClickFunc() {
   //TODO add your code here to get the desired functionality
   console.log('func: singleClickFunc');
   $('.cardImg', this).toggle();
-  // $('.cardImg', this).toggle(function() {
-  //   $('.cardImg', this).toggleClass('displayed');
-  // });
 }
---
+++ discussion.js
@@ -0,0 +1,9 @@
+$(document).ready(shoutOut);
+
+function shoutOut() {
+    console.log("My girl friend, Becky has been really helpful, letting me vent when I'm struggling or frustrated");
+    $('#frustration').remove();
+
+    console.log("My cohort mates have been awesome at helping me complete the assignments, especially making sure the tests pass. I frequently miss some small bit of syntax and fresh eyes are extremly helpful.");
+    $('#assignment').submit();
+}
