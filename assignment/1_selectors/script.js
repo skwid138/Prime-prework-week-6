6f13139f51106f8080e1eb77ba00787adf44c734
skwid138 <skwid138@gmail.com>
2017-08-27 11:47:11 -0500

    completed assignment 1

--- assignment/1_selectors/script.js
+++ assignment/1_selectors/script.js
@@ -7,24 +7,25 @@ var onReady = function(e, complete) { // ignore e, use complete when specified b
 
   // 1 - Select just the h1 inside the header element and
   // make it disappear with the `hide()` method
-
+  $('header').find('h1').hide();
   // 2 - Select the element with id "step-two" and
   // change its background color to yellow.
   // HINT: look up .css() to access an element's css properties
-
+  $('#step-two').css('background-color', 'yellow');
   // 3 - fade example-list elements to an opacity of 0.4
   // use complete as the last argument to the jQuery fade function
-
+  $('.example-list').fadeTo('slow', 0.4, complete);
   // 4 - Select all of the odd-numbered list items and
   // change their text color to blue
   // HINT: look up .css() to access an element's css properties
-
+  $('li:odd').css('color', 'blue');
   // 5 - select the last list item with a pseudo selector
   // write your code for question 5 here.
-
+  $('li:nth-last-child(1)').css('text-decoration', 'underline');
   // 6 - Using DOM traversal, select the last list item on the page
   // using any of the following: .parent(), .children(), .last().
   // Change its text color to red.
+  $('li').last().css('color', 'red');
 };
 
 // When the DOM is loaded, our code in the onReady function will run.
